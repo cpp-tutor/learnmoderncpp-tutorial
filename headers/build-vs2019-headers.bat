653aa6dd01b355b58c6369af4ef9b8cb16dd9ae6
Richard Spencer <99644930+cpp-tutor@users.noreply.github.com>
2022-10-14 17:07:14 +0100

    Delete build-vs2019-headers.bat

--- headers/build-vs2019-headers.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM This script will compile all files with .cpp extension in current directory
-REM Visual Studio 2019 (16.8) header file version
-
-set failures=0
-for %%f in (*.cpp) do echo %%f... && cl /std:c++latest /EHsc %%f >nul 2>nul || echo Failed to compile %%f && set /A failures=failures+1
-echo A total of %failures% files failed to compile.
