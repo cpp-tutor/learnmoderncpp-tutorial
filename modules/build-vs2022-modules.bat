7f05031a10150afe49bf6f36399c6341a05069d3
cpp-tutor <cpptutor@outlook.com>
2022-10-23 13:00:03 +0100

    Line endings to CRLF

--- modules/build-vs2022-modules.bat
+++ modules/build-vs2022-modules.bat
@@ -1,6 +1,6 @@
 @echo off
-REM This script will compile all files with .cpp extension in current directory
-REM Visual Studio 2022 (17.x) C++ modules version
+rem This script will compile all files with .cpp extension in current directory
+rem Visual Studio 2022 (17.x) C++ modules version
 
 set failures=0
 for %%f in (*.cpp) do echo %%f... && cl /std:c++latest /EHsc /experimental:module /MD %%f >nul 2>nul || echo Failed to compile %%f && set /A failures=failures+1
