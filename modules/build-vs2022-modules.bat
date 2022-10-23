8fd6a60c15fce140575cb30ea8bd6f1be01c9018
cpp-tutor <cpptutor@outlook.com>
2022-10-23 15:48:29 +0100

    Fix line endings

--- modules/build-vs2022-modules.bat
+++ modules/build-vs2022-modules.bat
@@ -3,5 +3,5 @@ rem This script will compile all files with .cpp extension in current directory
 rem Visual Studio 2022 (17.x) C++ modules version
 
 set failures=0
-for %%f in (*.cpp) do echo %%f... && cl /std:c++latest /EHsc /experimental:module /MD %%f >nul 2>nul || echo Failed to compile %%f && set /A failures=failures+1
+for %%f in (*.cpp) do echo %%f... && cl /std:c++latest /EHsc /experimental:module /MD %%f >nul 2>nul || echo Failed to compile %%f && set /A failures=failures+1 
 echo A total of %failures% files failed to compile.
