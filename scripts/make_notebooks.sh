f8757c8bec3786a6c91c64600ab0914193c6b2b3
cpp-tutor <cpptutor@outlook.com>
2025-10-30 14:54:08 +0000

    Update Jupyter notebooks for v1.2

--- scripts/make_notebooks.sh
+++ scripts/make_notebooks.sh
@@ -6,5 +6,5 @@ srcdir=$(dirname $(readlink -f "$0"))/..
 destdir=$srcdir/jupyter-notebooks
 for f in $srcdir/{01,02,03,04,05,06,07,08,09,10}-*.md ; do
   out=$destdir/$(basename $f)
-  cat $f | sed 's/```cpp/```python/' | jupytext --from md --to notebook -o ${out%%.md}.ipynb
+  cat $f | sed 's/```cpp/```python/' | jupytext --from md --to notebook -k cpp23 -o ${out%%.md}.ipynb
 done
